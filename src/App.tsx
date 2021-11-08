6f602a22a65b52b4a1df384b30f8c8bcc77599fc
Daniel Schubert <schubertmdaniel@gmail.com>
2021-11-07 23:45:01 -0800

    Improve section selection
    - Add enum `Title` with valid title names
    - Add `titles` array with titles in order
    - Add switch block to conditionally render sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 import Column from "./Column";
 import { generatePaths, scalePath } from "./path";
-
-import data from "./data.json";
+import { Title, titles } from "./constants";
 
 import "./styles.css";
+import Contact from "./pages/Contact";
 
 const placeholderText = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rhoncus nisi sed orci porttitor auctor. Suspendisse tempus eget sapien fermentum elementum. In ut luctus tortor. Aenean faucibus risus odio. Vivamus in viverra lorem, a gravida odio. Proin et rhoncus tortor. Integer feugiat urna sem, vitae molestie tellus posuere vitae. Nullam justo nisi, ultricies facilisis malesuada et, sollicitudin sit amet dolor. Cras id scelerisque orci, in aliquet neque. In nunc libero, tincidunt nec eros quis, laoreet dignissim metus. Curabitur aliquet lectus aliquet orci ornare, eu vulputate nisi dapibus. Sed ut tempus elit, at tempus quam. Suspendisse et malesuada ante. Donec auctor placerat ipsum non dictum. Morbi scelerisque erat sed porttitor mollis.
@@ -27,18 +27,26 @@ const limits = {
 };
 const repeatCount = 100;
 
-interface ColumnData {
-  title: string;
-  notes: Array<string>
-}
-
 const mql = window.matchMedia('(max-width: 600px)');
+
+const columnTitleToNode = (title: Title, smallScreen: boolean): ReactNode => {
+  switch (title) {
+    case Title.Aws:
+      return <p>AWS</p>
+    case Title.Personal:
+      return <p>Personal</p>
+    case Title.Contact:
+      return <Contact smallScreen={smallScreen} />
+    default:
+      return <p>{placeholderText}</p>
+  }
+};
 
 export default function App() {
   const [[topPath, leftPath]] = useState(() =>
     generatePaths(limits, repeatCount)
   );
-  const [selectedItem, setSelectedItem] = useState<ColumnData | null>(null);
+  const [selectedItem, setSelectedItem] = useState<Title | null>(null);
   const [smallScreen, setSmallScreen] = useState(mql.matches);
 
   useEffect(() => {
@@ -53,15 +61,15 @@ export default function App() {
 
   return (
     <div className="App">
-      {(data as Array<ColumnData>).map((el, i) => (
+      {(titles).map((title, i) => (
         <Column
           key={i}
-          text={el.title}
+          text={title}
           topPath={topPath}
           leftPath={leftPath}
           marginRight="random"
           visible={selectedItem === null}
-          onClick={() => setSelectedItem(el)}
+          onClick={() => setSelectedItem(title)}
         />
       ))}
 
@@ -69,14 +77,14 @@ export default function App() {
         selectedItem && (
           <div className="modal" style={{ flexDirection: smallScreen ? "column" : "row"}}>
             <Column
-              text={selectedItem.title}
+              text={selectedItem}
               topPath={smallScreen ? scalePath(topPath, 0.25) : topPath}
               leftPath={smallScreen ? scalePath(leftPath, 7) : new Array(repeatCount).fill(0)}
               marginRight="random"
               floating
               onClick={() => setSelectedItem(null)}
             />
-            <p>{placeholderText}</p>
+            {columnTitleToNode(selectedItem, smallScreen)}
           </div>
         )
       }
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+enum Title {
+  Aws = "AWS",
+  Personal = "PERSONAL",
+  Contact = "CONTACT"
+}
+
+/** Titles in order */
+const titles = [
+  Title.Aws,
+  Title.Personal,
+  Title.Contact
+];
+
+export { Title, titles };
